1873b39d463bc0e8a7c1487a4f123725ed117fec
Jonatan Koroch de Aguiar <jonatanaguiar1998@hotmail.com>
2021-01-27 21:13:52 -0300

    padronizando codigo com eslint airbnb

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,23 +1,20 @@
-import {Router} from 'express';
-
+import { Router } from 'express';
 import { uuid } from 'uuidv4';
- 
+
 const appointmentsRouter = Router();
-
 const appointments = [];
-
 appointmentsRouter.post('/', (request, response) => {
-    const { provider, date } = request.body;
-    
-    const appointment = {
-        id: uuid(),
-        provider,
-        date,
-    }
+  const { provider, date } = request.body;
 
-    appointments.push(appointment);
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
 
-    return response.json(appointment);
+  appointments.push(appointment);
+
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
