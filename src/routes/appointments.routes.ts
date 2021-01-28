89c8f23b1f7781b954d9050172b9bb80cf087516
Jonatan Koroch de Aguiar <jonatanaguiar1998@hotmail.com>
2021-01-27 22:59:43 -0300

    Desenvolvendo o agendamento e validacoes

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,15 +1,40 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable comma-dangle */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable indent */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable max-len */
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
-const appointments = [];
+
+interface Appointment{
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
+
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppontmentInSameDate = appointments.find((appointment) => 
+    isEqual(parsedDate, appointment.date)
+  );
+
+  if (findAppontmentInSameDate) {
+    return response.status(400).json({ message: 'This appointment is already booked' });
+  }
 
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
