db4232d2b4cfae086bfbad315342c074aefb767d
Jonatan Koroch de Aguiar <jonatanaguiar1998@hotmail.com>
2021-04-25 09:12:30 -0300

    Criando a parte de recuperação de senha

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -4,7 +4,7 @@ import User from '../../infra/typeorm/entities/User';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 import { uuid } from 'uuidv4';
 
-class UsersRepository implements IUserRepository {
+class FakeUsersRepository implements IUserRepository {
   private users: User[] = [];
 
   public async findById(id: string): Promise<User | undefined>{
@@ -33,4 +33,4 @@ class UsersRepository implements IUserRepository {
   }
 }
 
-export default UsersRepository;
+export default FakeUsersRepository;
