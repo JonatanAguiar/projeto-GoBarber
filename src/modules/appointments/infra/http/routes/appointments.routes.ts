89d19e4ff7c53c4cc7869c2b0233189713b5a089
Jonatan Koroch de Aguiar <jonatanaguiar1998@hotmail.com>
2022-01-30 01:50:31 -0300

    feat: Validating data in routes

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import { Router } from 'express';
 
@@ -12,7 +13,16 @@ const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post(
+    '/', 
+    celebrate({
+        [Segments.BODY]:{
+            provider_id: Joi.string().uuid().required(),
+            date: Joi.string().isoDate(),
+        },
+    }),
+    appointmentsController.create
+);
 appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 
--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import { Router } from 'express';
 
@@ -15,7 +16,23 @@ const providerDayAvailabilityController = new ProviderDayAvailabilityController(
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
-providersRouter.get('/:provider_id/month-availability', providerMonthAvailabilityController.index);
-providersRouter.get('/:provider_id/day-availability', providerDayAvailabilityController.index);
+providersRouter.get(
+    '/:provider_id/month-availability', 
+    celebrate({
+        [Segments.PARAMS]: {
+            provider_id: Joi.string().uuid().required(),
+        },
+    }), 
+    providerMonthAvailabilityController.index
+);
+providersRouter.get(
+    '/:provider_id/day-availability', 
+    celebrate({
+        [Segments.PARAMS]: {
+            provider_id: Joi.string().uuid().required(),
+        },
+    }), 
+    providerDayAvailabilityController.index
+);
 
 export default providersRouter;
